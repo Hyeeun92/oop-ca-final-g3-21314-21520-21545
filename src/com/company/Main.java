f35913254de7b7971c6dd0d542975431993e53db
Liubov Eremenko <eremenko.luba79@gmail.com>
2021-03-30 13:55:24 +0100

    luba login page

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,8 +13,20 @@ public class Main {
 
     */
 
-    public static void main(String[] args) {
+ /*   public static void main(String[] args) {
         Administrator administrator = new Administrator();
-    }
+    } */
+ public static void main(String[] args) {
+     // create an instance of ID and passwords class
+     IDandPasswords idandPasswords = new IDandPasswords();
+
+     //create an instance of login page
+     LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());  // set login info as argument, it will return hash map
+
+
+
+
+
+
 
 }
