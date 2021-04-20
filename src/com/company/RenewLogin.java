05699c2173ff2bf92ad4ab62b3e106d9dc7efd94
Liubov Eremenko <eremenko.luba79@gmail.com>
2021-04-20 13:47:32 +0100

    Commit added renewlogin + id block + db

--- src/com/company/RenewLogin.java
+++ src/com/company/RenewLogin.java
@@ -16,11 +16,13 @@ public class RenewLogin extends JFrame {
 
     JFrame frame;
     JButton btnSave;
-    JPasswordField oldField, newField, confirmField;
-    JLabel title, oldLabel, newLabel, confirmLabel, messageLabel;
-    String pswdOld = null;
+    JTextField idField;
+    JPasswordField pswdField, newField, confirmField;
+    JLabel title, idLabel, pswdLabel, newLabel, confirmLabel, messageLabel;
+    String pswd = null;
     String pswdNew = null;
     String pswdConfirm = null;
+    String id = null;
 
     public RenewLogin() {
 
@@ -37,19 +39,22 @@ public class RenewLogin extends JFrame {
 
         btnSave = new JButton("Save");
 
-        oldField = new JPasswordField(null);
+        idField = new JTextField(null);
+        pswdField = new JPasswordField(null);
         newField = new JPasswordField(null);
         confirmField = new JPasswordField(null);
 
-        oldLabel = new JLabel("Current password:");
+        idLabel = new JLabel("ID:");
+        pswdLabel = new JLabel("Current password:");
         newLabel = new JLabel("New password:");
         confirmLabel = new JLabel("Confirm new password:");
 
         messageLabel = new JLabel();
 
-
-        oldLabel.setBounds(100, 175, 150, 25);
-        oldLabel.setFont(new Font("Serif", Font.BOLD, 16));
+        idLabel.setBounds(100, 125, 150, 25);
+        idLabel.setFont(new Font("Serif", Font.BOLD, 16));
+        pswdLabel.setBounds(100, 175, 150, 25);
+        pswdLabel.setFont(new Font("Serif", Font.BOLD, 16));
         newLabel.setBounds(100, 225, 150, 25);
         newLabel.setFont(new Font("Serif", Font.BOLD, 16));
         confirmLabel.setBounds(100, 275, 170, 25);
@@ -59,7 +64,8 @@ public class RenewLogin extends JFrame {
         messageLabel.setBounds(325, 250, 250, 35);
         messageLabel.setFont(new Font(null, Font.BOLD, 25));
 
-        oldField.setBounds(100, 199, 300, 25);
+        idField.setBounds(100, 149, 300, 25);
+        pswdField.setBounds(100, 199, 300, 25);
         newField.setBounds(100, 249, 300, 25);
         confirmField.setBounds(100,299,300, 25);
 
@@ -70,14 +76,14 @@ public class RenewLogin extends JFrame {
 
 
         frame.add(title);
-        frame.add(idLable);
-        frame.add(oldLabel);
+        frame.add(idLabel);
+        frame.add(pswdLabel);
         frame.add(newLabel);
         frame.add(confirmLabel);
         frame.add(messageLabel);
 
-        frame.add(idField)
-        frame.add(oldField);
+        frame.add(idField);
+        frame.add(pswdField);
         frame.add(newField);
         frame.add(confirmField);
         frame.add(btnSave);
@@ -89,24 +95,63 @@ public class RenewLogin extends JFrame {
         frame.setLayout(null);
         frame.setVisible(true);
 
-        // btnSave.addActionListener(
-        //      new ActionListener() {
-        //         @Override
-        //         public void actionPerformed(ActionEvent e) {
+        btnSave.addActionListener(
+               new ActionListener() {
+                  @Override
+                  public void actionPerformed(ActionEvent e) {
+                        String id, pswdOld, pswdNew, pswdConfirm;
+                        id = idField.getText();
+                        pswd = pswdField.getPassword();
+                        pswdNew = newField.getPassword();
+                        pswdConfirm = confirmField.getPassword();
+                        String db1 = "select * from student where student_id = '"+id+"' ";
+                        String db2 = "update student set student_password ='"+pswdNew+"' where student_id = '"+id+"' ";
 
-        //           id = oldField.getPassword();
-        //            pswd = String.valueOf(newField.getPassword());
+                  if (pswdNew.equals(pswdConfirm)){
 
-        //         if (adminRadio.isSelected()) {
-        //          db.getAdminLoginInfo(id, pswd);
-        //       } else if (lectureRadio.isSelected()) {
-        //          db.getLectureLoginInfo(id, pswd);
-        //    } else if (studentRadio.isSelected()) {
-        //      db.getStudentLoginInfo(id,pswd);
-        //     }
-        //  }
-        //}
-        //);
+                            try {
+
+
+
+
+                                st=con.prepareStatement(db.getStudentLoginInfo(id, pswd);
+                                if() {
+                                    st=con.prepareStatement("update login set password = ? where id = ?" );
+                                    st.setString( , pswdNew);
+                                    st.setString( , id);
+                                    JOptionPane.showMessageDialog(null, "Password Changed Successfully");
+                                                db //set new pswd to
+                                                    id.setText("");
+                                                    pswd ;
+                                                    pswdNew;
+                                                    pswdConfirm ;
+
+                                }else {
+                                    JOptionPane.showMessageDialog(null, "Invalid ID or Old password");
+
+                                } catch (Exception e) {
+                                    System.out.println(e);
+                                }
+                                }
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "New Password does not match with Confirm Password");
+
+                        }
+
+                        id = pswdField.getPassword();
+                        pswd = String.valueOf(newField.getPassword());
+
+                        /*if (adminRadio.isSelected()) {
+                           db.getAdminLoginInfo(id, pswd);
+                        } else if (lectureRadio.isSelected()) {
+                            db.getLectureLoginInfo(id, pswd);
+                        } else if (studentRadio.isSelected()) {
+                          db.getStudentLoginInfo(id,pswd);*/
+                       }
+                    }
+               }
+        );
     }
 
 
